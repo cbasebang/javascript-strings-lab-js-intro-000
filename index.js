333671e0f0f4fc5912a67f6805e07acb013eabbf
Claude Funge <bclaudefunge@gmail.com>
2020-08-02 15:10:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = ('Hello, ${specialGuest}!')
-
+var greetSpecialGuest = "" + specialGuest + " ! ";
+"Hello, + ${specialGuest} + "!"
 var topic = "space"
 
 var conversation = ("Let's talk about ${space}.")
